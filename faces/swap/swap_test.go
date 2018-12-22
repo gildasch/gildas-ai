2526fee232cec129be7afbb61c920c7ca18841bb
Gildas Chabot <gildas.chabot@schibsted.com>
2018-12-22 13:42:14 +0100

    [tests] remove output images in faces/swap

--- faces/swap/swap_test.go
+++ faces/swap/swap_test.go
@@ -1,14 +1,19 @@
 package swap
 
 import (
+	"bytes"
+	"fmt"
 	"image"
+	"image/png"
+	"io/ioutil"
 	"testing"
+	"time"
 
-	"github.com/fogleman/gg"
 	"github.com/gildasch/gildas-ai/faces"
 	"github.com/gildasch/gildas-ai/faces/detection"
 	"github.com/gildasch/gildas-ai/faces/landmarks"
 	"github.com/gildasch/gildas-ai/imageutils"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -32,7 +37,7 @@ func TestFaceSwap(t *testing.T) {
 	out, err := FaceSwap(extractor, landmark, dest, src, 0)
 	require.NoError(t, err)
 
-	gg.SavePNG("out-faceswap.png", out)
+	assertImageEqual(t, "faceswap-expected.png", out)
 }
 
 func TestSwap(t *testing.T) {
@@ -47,7 +52,7 @@ func TestSwap(t *testing.T) {
 	out, err := swap(landmark, src, dest, 0)
 	require.NoError(t, err)
 
-	gg.SavePNG("out-swap.png", out)
+	assertImageEqual(t, "swap-expected.png", out)
 }
 
 func TestMaskFromPolygon(t *testing.T) {
@@ -58,7 +63,7 @@ func TestMaskFromPolygon(t *testing.T) {
 		{10, 10}, {5, 65}, {17, 85}, {45, 66}, {70, 70}, {60, 30}, {50, 15},
 	})
 
-	gg.SavePNG("out.png", mask)
+	assertImageEqual(t, "maskFromPolygon-expected.png", mask)
 }
 
 func TestMaskFromPolygonWithBounds(t *testing.T) {
@@ -68,8 +73,31 @@ func TestMaskFromPolygonWithBounds(t *testing.T) {
 	})
 	mask := maskFromPolygon(in, []image.Point{
 		{110, 110}, {105, 165}, {117, 185}, {145, 166}, {170, 170}, {160, 130}, {150, 115},
-		// {10, 10}, {5, 65}, {17, 85}, {45, 66}, {70, 70}, {60, 30}, {50, 15},
 	})
 
-	gg.SavePNG("out2.png", mask)
+	assertImageEqual(t, "maskFromPolygonWithBounds-expected.png", mask)
+}
+
+func assertImageEqual(t *testing.T, expectedFilename string, actual image.Image) bool {
+	start := time.Now()
+	defer func() {
+		fmt.Println("assertImageEqual in ", time.Since(start))
+	}()
+
+	expectedBytes, err := ioutil.ReadFile(expectedFilename)
+	if err != nil {
+		t.Errorf("could not read expected image %q: %v", expectedFilename, err)
+		t.FailNow()
+		return false
+	}
+
+	var actualBytes bytes.Buffer
+	err = png.Encode(&actualBytes, actual)
+	if err != nil {
+		t.Errorf("could not encode actual image: %v", err)
+		t.FailNow()
+		return false
+	}
+
+	return assert.Equal(t, expectedBytes, actualBytes.Bytes())
 }
