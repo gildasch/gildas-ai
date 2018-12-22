fda69cf42268574f4575345c7d4c52bbf20b66a2
Gildas Chabot <gildas.chabot@schibsted.com>
2018-12-22 14:47:30 +0100

    [tests] remove outputs and add image checks on faces/landmarks

--- faces/landmarks/landmarks_test.go
+++ faces/landmarks/landmarks_test.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"image"
 	"image/draw"
-	"image/jpeg"
-	"os"
 	"testing"
 
 	"github.com/gildasch/gildas-ai/faces/detection"
@@ -18,70 +16,15 @@ func TestDetection(t *testing.T) {
 	require.NoError(t, err)
 
 	for i := 1; i < 12; i++ {
-		testImage, err := imageutils.FromFile(fmt.Sprintf("%d.jpg", i))
+		testImage, err := imageutils.FromFile(fmt.Sprintf("testdata/single-%d.jpg", i))
 		require.NoError(t, err)
 
 		landmarks, err := l.Detect(testImage)
 		require.NoError(t, err)
 
-		saveLandmarksImage(testImage, landmarks, fmt.Sprintf("%d-marked.jpg", i))
+		withLandmarks := landmarks.DrawOnImage(testImage)
+		imageutils.AssertImageEqual(t, fmt.Sprintf("expected/single-%d-marked.png", i), withLandmarks)
 	}
-}
-
-func saveLandmarksImage(img image.Image, landmarks *Landmarks, filename string) {
-	withLandmarks := landmarks.DrawOnImage(img)
-
-	outf, _ := os.Create(filename)
-	jpeg.Encode(outf, withLandmarks, nil)
-}
-
-func TestGenerateData(t *testing.T) {
-	l, err := NewLandmark()
-	require.NoError(t, err)
-
-	for _, i := range []string{"a", "b", "c", "d"} {
-		testImage, err := imageutils.FromFile(fmt.Sprintf("%s.png", i))
-		require.NoError(t, err)
-
-		landmarks, err := l.Detect(testImage)
-		require.NoError(t, err)
-
-		cropped := landmarks.Center(testImage, testImage)
-		outf, _ := os.Create(fmt.Sprintf("%s-cropped.jpg", i))
-		jpeg.Encode(outf, cropped, nil)
-	}
-}
-
-func TestGenerateData2(t *testing.T) {
-	l, err := NewLandmark()
-	require.NoError(t, err)
-
-	for i := 1; i <= 11; i++ {
-		testImage, err := imageutils.FromFile(fmt.Sprintf("%d.jpg", i))
-		require.NoError(t, err)
-
-		landmarks, err := l.Detect(testImage)
-		require.NoError(t, err)
-
-		cropped := landmarks.Center(testImage, testImage)
-		outf, _ := os.Create(fmt.Sprintf("%d-cropped.jpg", i))
-		jpeg.Encode(outf, cropped, nil)
-	}
-}
-
-func TestGenerateData3(t *testing.T) {
-	l, err := NewLandmark()
-	require.NoError(t, err)
-
-	testImage, err := imageutils.FromFile("4-face-1.jpg")
-	require.NoError(t, err)
-
-	landmarks, err := l.Detect(testImage)
-	require.NoError(t, err)
-
-	cropped := landmarks.Center(testImage, testImage)
-	outf, _ := os.Create("4-face-1-cropped.jpg")
-	jpeg.Encode(outf, cropped, nil)
 }
 
 func TestFullImage(t *testing.T) {
@@ -112,12 +55,9 @@ func TestFullImage(t *testing.T) {
 		require.NoError(t, err)
 
 		withLandmarks := landmarks.DrawOnFullImage(b, testImage)
-
-		outf, _ := os.Create(fmt.Sprintf("2-%d-marked.jpg", i))
-		jpeg.Encode(outf, withLandmarks, nil)
+		imageutils.AssertImageEqual(t, fmt.Sprintf("expected/2-%d-marked.png", i), withLandmarks)
 
 		cropped := landmarks.Center(b, testImage)
-		outf2, _ := os.Create(fmt.Sprintf("2-%d-cropped.jpg", i))
-		jpeg.Encode(outf2, cropped, nil)
+		imageutils.AssertImageEqual(t, fmt.Sprintf("expected/2-%d-cropped.png", i), cropped)
 	}
 }
